9ecb89d725cbd4a078f19958e183bb7c69024264
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-08-14 21:41:51 +0100

    WIP Refactor to single method

--- src/main/kotlin/ConnectFour.kt
+++ src/main/kotlin/ConnectFour.kt
@@ -64,36 +64,36 @@ class ConnectFour {
         return transposedListOfStrings.toTypedArray()
     }
 
-    private fun checkForDiagonalWin(grid: Array<String>, player: Char): Boolean {
-        var yellowWin = false
-        val gridAsSingleString = grid.joinToString("").toLowerCase()
+    private fun checkForWin(grid: Array<String>, player: Char, direction: String): Boolean {
+        var win = false
+        var increment = 0
+        when {
+            direction == "dRight" -> increment = 8
+            direction == "dLeft" -> increment = 6 // this is failing as an increment of 6 can be on the same line! All of these are flawed, as they could all wrap over aline break
+            direction == "h" -> increment = 0
+            direction == "v" -> increment = 7 // fails for above reasons
+        }
 
-        for ((index, char) in gridAsSingleString.withIndex()) {
-            if (char == player && index < 18) {
-                if (gridAsSingleString.toList()[index + 8] == player
-                    && gridAsSingleString.toList()[index + 16] == player
-                    && gridAsSingleString.toList()[index + 24] == player) {
-                    yellowWin = true
+        val gridString = grid.joinToString("").toLowerCase().toList()
+
+        for ((i, char) in gridString.withIndex()) {
+            if (i + (3 * (increment)) < gridString.size) {
+                when {
+                    char == player
+                        && gridString[i + increment] == player
+                        && gridString[i + (2 * (increment))] == player
+                        && gridString[i + (3 * (increment))] == player -> win = true
                 }
             }
         }
-
-        for ((index, char) in gridAsSingleString.withIndex()) {
-            if (char == player && index < 21) {
-                if (gridAsSingleString.toList()[index + 6] == player
-                    && gridAsSingleString.toList()[index + 12] == player
-                    && gridAsSingleString.toList()[index + 18] == player) {
-                    yellowWin = true
-                }
-            }
-        }
-        return yellowWin
+        return win
     }
 
     fun getGridStatus(grid: Array<String>): String {
         return when {
-            checkForRedWin(grid) || checkForRedColumnWin(grid) || checkForDiagonalWin(grid, 'r') -> "Red wins"
-            checkForYellowWin(grid) || checkForYellowColumnWin(grid) || checkForDiagonalWin(grid, 'y') -> "Yellow wins"
+//            checkForRedWin(grid) || checkForRedColumnWin(grid) || checkForWin(grid, 'r', "dRight") || checkForWin(grid, 'r', "dLeft") -> "Red wins"
+            checkForRedWin(grid) || checkForWin(grid, 'r', "v") || checkForWin(grid, 'r', "dRight") || checkForWin(grid, 'r', "dLeft") -> "Red wins"
+            checkForYellowWin(grid) || checkForYellowColumnWin(grid) || checkForWin(grid, 'y', "dRight") || checkForWin(grid, 'y', "dLeft") -> "Yellow wins"
             checkForBlanks(grid) && checkForR(grid) -> "Yellow plays next"
             checkForBlanks(grid) && !checkForR(grid) -> "Red plays next"
             else -> "Draw"
